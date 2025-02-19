076a7f0534da34e4664e231dd5c8108fafc0adc2
Carter Haney <carterleehaney@gmail.com>
2025-02-18 20:13:24 -0600

    background + cyberforce

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ function timeUntil(targetDate) {
 }
 
 function updateCountdowns() {
-    document.getElementById("days1").innerText = timeUntil('2025-02-09');
-    document.getElementById("days2").innerText = timeUntil('2025-02-29');
-    document.getElementById("days3").innerText = timeUntil('2025-03-22');
+    document.getElementById("days2").innerText = timeUntil('2025-02-28'); // Changed date
+    document.getElementById("days3").innerText = timeUntil('2025-03-21');
     document.getElementById("days4").innerText = timeUntil('2025-04-26');
-    document.getElementById("days5").innerText = timeUntil('2025-10-03');
+    document.getElementById("days5").innerText = timeUntil('2025-10-02');
+    document.getElementById("days6").innerText = timeUntil('2025-11-14');
 }
 
 // Initial call to update countdowns
